5e17481068c03bd33722c29d447570da2c29b858
binodsubedi125 <binodsubedi125@gmail.com>
2024-07-22 13:36:12 +0545

    initial structure created

---
+++ privateChefBackendS/src/server.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json())
+
+const port = process.env.PORT || 5000
+
+app.listen(port,()=>{
+    console.log(`Server is listening at port ${port}....`)
+})

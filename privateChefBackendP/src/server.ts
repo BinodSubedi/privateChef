6f39dea701d7d1ed734bd715f4be23d6174bacd6
binodsubedi125 <binodsubedi125@gmail.com>
2024-07-23 10:38:10 +0545

    setting up the env files for both dev and production

--- privateChefBackendP/src/server.ts
+++ privateChefBackendP/src/server.ts
@@ -1,10 +1,11 @@
 import express from "express";
+import env from "./config";
 
 const app = express();
 
 app.use(express.json());
 
-const port = process.env.PORT || 3000;
+const port = env.port;
 
 app.listen(port, () => {
   console.log(`Server is listening at port ${port}....`);
